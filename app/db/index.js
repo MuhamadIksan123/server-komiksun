cb7f32d336f893d8a6f53f699a8a92a943dea79b
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-07-03 16:59:13 +0700

    hapus chapter dan install mongodb

--- app/db/index.js
+++ app/db/index.js
@@ -5,10 +5,23 @@ const mongoose = require('mongoose');
 const {urlDb} = require('../config');
 
 // connect ke mongoDb menggunakan konfigurasi yang telah kita import mongoose.connect(urlDb);
-mongoose.connect(urlDb);
+mongoose
+  .connect(urlDb, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection is successful');
+  })
+  .catch((error) => {
+    console.error('Error is connection to mongo atlas database', error);
+  });
+  
 
 // simpan kondek dalam constant db
 const db = mongoose.connection;
 
 // export db supaya bisa digunakan oleh file lain yang membutuhkan
 module.exports = db;
+
+
