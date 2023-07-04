84517f636f9b6c61af443de7856bfd8b3564e7be
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-07-04 20:01:44 +0700

    update response db

--- app/db/index.js
+++ app/db/index.js
@@ -2,26 +2,16 @@
 const mongoose = require('mongoose');
 
 // kita import konfigurasi terkait mongoDb dari app/config.js
-const {urlDb} = require('../config');
+const { urlDb } = require('../config');
 
 // connect ke mongoDb menggunakan konfigurasi yang telah kita import mongoose.connect(urlDb);
-mongoose
-  .connect(urlDb, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('DB connection is successful');
-  })
-  .catch((error) => {
-    console.error('Error is connection to mongo atlas database', error);
-  });
-  
+mongoose.connect(urlDb, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // simpan kondek dalam constant db
 const db = mongoose.connection;
 
 // export db supaya bisa digunakan oleh file lain yang membutuhkan
 module.exports = db;
-
-
